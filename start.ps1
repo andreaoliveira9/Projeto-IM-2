dfdf4e1b73c192e06ec4ce6f82a45daf6366758e
André Oliveira <andreaoliveira@ua.pt>
2024-12-10 15:27:16 +0000

    start

--- start.ps1
+++ start.ps1
@@ -1,14 +1,18 @@
-Set-Location -Path .\mmiframeworkV2
+Set-Location -Path .\IM
 Start-Process -FilePath .\start.bat
 
 Set-Location -Path ..\FusionEngine
 Start-Process -FilePath .\start.bat
 
-Set-Location -Path ..
-conda activate rasa-env
-Set-Location -Path .\rasaDemo
-Start-Process -FilePath rasa -ArgumentList 'run', '--enable-api', '-m', '.\models\', '--cors', '*'
+Set-Location -Path ..\GenericGesturesModality-2023
+Start-Process -FilePath .\GenericGesturesModality.exe
 
 Set-Location -Path ..\WebAppAssistantV2
 Start-Process -FilePath .\start_web_app.bat
+
 Set-Location -Path ..
+Start-Sleep -Seconds 3
+Start-Process "chrome.exe" "https://127.0.0.1:8082/index.htm"
+
+.\venv\Scripts\activate
+python.exe .\app\main.py
